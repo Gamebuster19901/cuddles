a8fe2db2a10a80038a9e6c5d3928802568a98d4d
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-10-13 13:00:05 -0400

    Remove examplemod stuff, add license, etc.

---
+++ src/main/java/com/gamebuster19901/cuddles/Main.java
@@ -0,0 +1,14 @@
+package com.gamebuster19901.cuddles;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("cuddles");
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
